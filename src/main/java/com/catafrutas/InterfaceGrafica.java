e136e05125922f3b259d046c99a94488bd98a936
lucasdv120105 <lucasdv1215@academico.ufs.br>
2024-10-03 15:57:38 -0300

    Classes e codigos java

---
+++ src/main/java/com/catafrutas/InterfaceGrafica.java
@@ -0,0 +1,10 @@
+package src.main.java.com.catafrutas;
+
+public class InterfaceGrafica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/catafrutas/Jogo.java
@@ -0,0 +1,10 @@
+package src.main.java.com.catafrutas;
+
+public class Jogo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
